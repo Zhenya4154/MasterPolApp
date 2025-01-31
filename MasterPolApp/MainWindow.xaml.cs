c608c3c3b80521082d40051dd9d02c516ed1d77d
Студенты Лисавиной А.В. <lisav@teach.local>
2025-01-31 14:46:03 +0300

    MainWindowMarking

--- MasterPolApp/MainWindow.xaml.cs
+++ MasterPolApp/MainWindow.xaml.cs
@@ -23,6 +23,8 @@ namespace MasterPolApp
         public MainWindow()
         {
             InitializeComponent();
+            Classes.Manager.MainFrame = MainFrame;
+            Classes.Manager.MainFrame.Navigate(new Pages.ListViewPage());
         }
     }
 }
